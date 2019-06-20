4df494d21069f2b32b20701a97d504d0f932728b
vladimir.kurov <vladimir.kurov@simbirsoft.com>
2019-06-20 22:44:55 +0400

    master

--- app/converter/NewsClient.scala
+++ app/converter/NewsClient.scala
@@ -7,7 +7,7 @@ import play.api.libs.ws.{WSClient, WSRequest, WSResponse}
 import scala.concurrent.{ExecutionContext, Future}
 
 class NewsClient @Inject()(ws: WSClient)(implicit ec: ExecutionContext) {
-  val url ="http://sonetmon.herokuapp.com/api/v1/news/search"
+  val url ="http://192.168.1.132:8083/api/v1/news/search"
 
   def fetch(query: String)(implicit mc: MarkerContext): Future[WSResponse] = {
     val request: WSRequest = ws.url(url)
